5613c06984cce7b90df775be188ea75180d09af4
kuroneko913 <myblackcat7112@gmail.com>
2020-06-07 19:07:31 +0900

    [add] auth test

---
+++ tests/Feature/RegisterApiTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\User;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class RegisterApiTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * @test
+     */
+    public function should_新しいユーザーを作成して返却する()
+    {
+        $data = [
+            'name' => 'photo-app user',
+            'email' => 'dummy@dmail.com',
+            'password' => 'Password1234!',
+            'password_confirmation' => 'Password1234!',
+        ];
+
+        $response = $this->json('POST', route('register'), $data);
+
+        $user = User::first();
+        $this->assertSame($data['name'], $user->name);
+
+        $response
+            ->assertStatus(201)
+            ->assertJson(['name' => $user->name]);
+    }
+}
